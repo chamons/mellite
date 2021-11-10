3ff59b193cbc18a419bced885cbceef0f8f351b6
Chris Hamons <chris.hamons@xamarin.com>
2021-11-10 15:19:31 -0600

    Add a bit to sample

--- sample/binding.cs
+++ sample/binding.cs
@@ -1,10 +1,13 @@
-﻿using System;
+using System;
 using ObjCRuntime;
 
 namespace binding {
 	[Introduced (PlatformName.iOS, 10, 0)]
 	[Introduced (PlatformName.MacOSX, 10, 0)]
 	public partial class Class1 {
+		[Deprecated (PlatformName.iOS, 13, 0)]
+		[Obsolete (PlatformName.MacOSX, 14, 0)]
+		[Unavailable (PlatformName.MacCatalyst)]
 		public void Foo () { }
 
 		[Introduced (PlatformName.iOS, 11, 0)]
