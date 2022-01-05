33d2c36961fbb4bd109b357ce95e30edb6e41b5e
Chris Hamons <chris.hamons@xamarin.com>
2022-01-05 13:17:28 -0600

    Update some scripts

--- scripts/why.sh
+++ scripts/why.sh
@@ -1,1 +1,3 @@
-dotnet run --project ../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1 --detect-unresolvable --verbose-conditional=$2
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1 --detect-unresolvable --verbose-conditional=$2
