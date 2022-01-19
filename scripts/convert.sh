9bad70dfe0d86d93070af22089515b969575a601
Chris Hamons <chris.hamons@xamarin.com>
2022-01-19 12:42:57 -0600

    Improve multi-pathing for convert script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,16 +5,17 @@ set -u
 set -o pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SDK_PATH=~/Programming/xamarin-macios/src/$1/
 
 echo "Checking for unresolvable first..."
-dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1/ --detect-unresolvable
+dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --detect-unresolvable
 echo "Clear. Stripping Existing NET6 Attributes..."
-dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1/ --strip-attributes
+dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --strip-attributes
 echo "Atttribute Strip Complete. Stripping unnecessary blocks..."
-dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1/ --strip-blocks
+dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --strip-blocks
 echo "Completed - Please review and commit."
 read _
 echo "Running final conversion..."
-dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build ~/Programming/xamarin-macios/src/$1/ --harvest-assembly=/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll
+dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --harvest-assembly=/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll
 echo "Completed - Please review and commit."
 read _
