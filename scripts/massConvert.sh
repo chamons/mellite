c2c321a2fabca368cdaa6b7a471c534219041fa2
Chris Hamons <chris.hamons@xamarin.com>
2022-02-22 14:17:25 -0600

    Minor script updates

--- scripts/massConvert.sh
+++ scripts/massConvert.sh
@@ -2,13 +2,19 @@
 
 
 # Skip AppKit ObjCRuntime
-#  "ARKit" "AVFoundation" "AVKit" "Accelerate" "Accessibility" "Accounts" "AdSupport" "AddressBook" "AddressBookUI" "AppKit" "AssetsLibrary" "AudioToolbox" "AudioUnit" "AuthenticationServices" "BackgroundTasks" "BusinessChat" "CFNetwork" "CallKit" "CarPlay" "Carbon" "Chip" "ClassKit" "ClockKit" "CloudKit" "Compression" "Contacts" "CoreAnimation" "CoreBluetooth" "CoreData" "CoreFoundation" "CoreGraphics" "CoreHaptics" "CoreImage" "CoreLocation" "CoreML" "CoreMedia" "CoreMidi" "CoreMotion" "CoreServices" "CoreSpotlight" "CoreTelephony" "CoreText" "CoreVideo" "CoreWlan" "Darwin" "EventKit" "EventKitUI" "ExternalAccessory" "FileProvider" "FinderSync" "Foundation" "GLKit" "GameController" "GameKit" "GameplayKit" "HealthKit" "HomeKit" "IOSurface" "ImageCaptureCore" "ImageIO" "ImageKit" "InputMethodKit" "Intents" "JavaScriptCore" "LocalAuthentication" "MLCompute" "MapKit" "MediaAccessibility" "MediaLibrary" "MediaPlayer" "MediaToolbox" "MessageUI"
+# "ARKit" "AVFoundation" "AVKit" "Accelerate" "Accessibility" "Accounts" "AdSupport" "AddressBook" "AddressBookUI" 
+# "AppKit" "AssetsLibrary" "AudioToolbox" 
+# "AudioUnit" "AuthenticationServices" "BackgroundTasks" "BusinessChat" "CFNetwork" "CallKit" "CarPlay" "Carbon" "Chip" "ClassKit" "ClockKit" "CloudKit" "Compression" "Contacts" "CoreAnimation" "CoreBluetooth" "CoreData" "CoreFoundation" "CoreGraphics" "CoreHaptics" "CoreImage" "CoreLocation" "CoreML" "CoreMedia" "CoreMidi" "CoreMotion" "CoreServices" "CoreSpotlight" "CoreTelephony" "CoreText" "CoreVideo" "CoreWlan" "Darwin" "EventKit" "EventKitUI" "ExternalAccessory" "FileProvider" "FinderSync"
+#  "Foundation" "GLKit" "GameController" "GameKit" "GameplayKit" "HealthKit" 
+# "HomeKit" "IOSurface" "ImageCaptureCore" "ImageIO" "ImageKit" "InputMethodKit" "Intents" "JavaScriptCore" "LocalAuthentication" "MLCompute" "MapKit" "MediaAccessibility" "MediaLibrary" "MediaPlayer" "MediaToolbox" "MessageUI"
 # "Metal" "MetalKit" "MetalPerformanceShaders" "MetricKit" "MobileCoreServices" "ModelIO" "MultipeerConnectivity" "NativeTypes" "NaturalLanguage" "NearbyInteraction" "Network" "NetworkExtension" "NewsstandKit" "NotificationCenter"  
 # "OpenGL" "OpenGLES" "PassKit" "PdfKit" "Photos" "PhotosUI"
-# "PrintCore" "QTKit" "QuickLook" "QuickLookUI" "ReplayKit" "SafariServices"
-# "SceneKit" "ScriptingBridge" "SearchKit"
-#  "Security" "SensorKit" "Simd" "Social" "SpriteKit" "StoreKit" "TVMLKit" "TVServices" "Twitter" 
-declare -a apis=("UIKit" "UserNotifications" "VideoSubscriberAccount" "VideoToolbox" "Vision" "WKWebKit" "WatchConnectivity" "WatchKit" "WebKit" "XKit" "iAd" "iTunesLibrary")
+#  "PrintCore" "QTKit" "QuickLook" "QuickLookUI" "ReplayKit" "SafariServices"
+# "SceneKit" "ScriptingBridge" "SearchKit" 
+# "Security" "SensorKit" "Simd" "Social" "SpriteKit" "StoreKit" "TVMLKit" "TVServices" "Twitter"
+#  "UIKit" "UserNotifications" "VideoSubscriberAccount" "VideoToolbox" "Vision" "WKWebKit" "WatchConnectivity" "WatchKit" "WebKit" "XKit" "iAd" "iTunesLibrary""
+# 
+declare -a apis=("AppKit")
 
 set -e
 set -u
@@ -25,5 +31,5 @@ do
    dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --detect-unresolvable
    dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --strip-attributes
    dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --strip-blocks
-   dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --harvest-assembly=Xamarin.iOS.dll --add-default-introduced
+   dotnet run --project $SCRIPT_DIR/../src/mellite.csproj -- --ignore=build $SDK_PATH --harvest-assembly=Xamarin.Mac.dll --add-default-introduced=$SCRIPT_DIR/../
 done
